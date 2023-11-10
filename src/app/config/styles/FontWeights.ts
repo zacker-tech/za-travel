fdeca459e7271d7d5f715b2d2a9122ec152c9114
zacker-tech <127632508+zacker-tech@users.noreply.github.com>
2023-11-10 14:45:58 +0100

    feat: added mui setup

---
+++ src/app/config/styles/FontWeights.ts
@@ -0,0 +1,6 @@
+export const FontWeights = {
+  regular: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import path from 'path';
+import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@features": path.resolve(__dirname, "./src/features"),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@config': path.resolve(__dirname, './src/app/config'),
     },
   },
   plugins: [react()],
