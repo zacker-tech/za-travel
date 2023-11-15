aa1762362ab73da923dc5f4151975d30db2da59c
zacker-tech <127632508+zacker-tech@users.noreply.github.com>
2023-11-15 23:08:09 +0100

    chore: improve auth api code

--- src/app/services/api/auth.ts
+++ src/app/services/api/auth.ts
@@ -7,20 +7,7 @@ import {
 
 import { auth, mapAuthCodeToMessage } from '@services/firebase';
 
-async function handleAuthError(authFunction: () => Promise<void>) {
-  try {
-    await authFunction();
-  } catch (error) {
-    if (error instanceof FirebaseError) {
-      console.log(error, 'error');
-      throw Error(mapAuthCodeToMessage(error.code));
-    }
-
-    throw Error('Something went wrong! Please try again!');
-  }
-}
-
-export async function register(name: string, email: string, password: string) {
+export function register(name: string, email: string, password: string) {
   return handleAuthError(async () => {
     await createUserWithEmailAndPassword(auth, email, password);
     if (auth.currentUser) {
@@ -31,8 +18,20 @@ export async function register(name: string, email: string, password: string) {
   });
 }
 
-export async function login(email: string, password: string) {
+export function login(email: string, password: string) {
   return handleAuthError(async () => {
     await signInWithEmailAndPassword(auth, email, password);
   });
+}
+
+async function handleAuthError(authFunction: () => Promise<void>) {
+  try {
+    await authFunction();
+  } catch (error) {
+    if (error instanceof FirebaseError) {
+      throw Error(mapAuthCodeToMessage(error.code));
+    }
+
+    throw Error('Something went wrong! Please try again!');
+  }
 }
