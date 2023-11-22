860cb2d3e9d3ab47192fe2d3e0f1db993cff8c69
zacker-tech <127632508+zacker-tech@users.noreply.github.com>
2023-11-22 17:21:57 +0100

    fix: sidebar close/open state

--- src/app/hooks/useBreakpoints.ts
+++ src/app/hooks/useBreakpoints.ts
@@ -3,9 +3,10 @@ import { useMediaQuery } from '@mui/material';
 import { theme } from '@config/styles';
 
 export function useBreakpoints() {
-  const sm = useMediaQuery(theme.breakpoints.up('md'));
+  const sm = useMediaQuery(theme.breakpoints.up('sm'));
   const md = useMediaQuery(theme.breakpoints.up('md'));
-  const lg = useMediaQuery(theme.breakpoints.up('md'));
+  const lg = useMediaQuery(theme.breakpoints.up('lg'));
+  const xl = useMediaQuery(theme.breakpoints.up('xl'));
 
-  return { sm, md, lg };
+  return { sm, md, lg, xl };
 }
--- src/features/ui/layout/AccountLayout/AccountLayout.tsx
+++ src/features/ui/layout/AccountLayout/AccountLayout.tsx
@@ -62,8 +62,8 @@ const StyledDrawer = styled(Drawer, {
 const TOOLBAR_STYLES = { mt: 2, mb: 1 };
 
 export default function AccountLayout() {
-  const { md } = useBreakpoints();
-  const [isOpen, setOpen] = useState(true);
+  const { md, xl } = useBreakpoints();
+  const [isOpen, setOpen] = useState(xl);
 
   const closeDrawer = () => {
     setOpen(false);
@@ -112,7 +112,7 @@ export default function AccountLayout() {
         <>
           <AppBar
             position="fixed"
-            sx={{ boxShadow: 'none', background: 'transparent' }}
+            sx={{ boxShadow: 'none', backgroundColor: 'grey.100' }}
           >
             <Toolbar sx={TOOLBAR_STYLES}>
               <IconButton
