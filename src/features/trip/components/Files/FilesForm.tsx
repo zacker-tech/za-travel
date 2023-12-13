054d52c0bccb4226636de637d23888e951854bc5
zacker-tech <127632508+zacker-tech@users.noreply.github.com>
2023-12-13 19:40:58 +0100

    feat: limit photos count to 5

--- src/features/trip/components/Files/FilesForm.tsx
+++ src/features/trip/components/Files/FilesForm.tsx
@@ -16,6 +16,7 @@ import {
   ACCEPTED_DOCUMENT_FORMATS,
   ACCEPTED_PHOTO_FORMATS,
   MAX_FILE_SIZE_MB,
+  MAX_TRIP_PHOTOS,
 } from '../../constants';
 import type { DocumentToUpload, TripFile } from '../../types';
 import DocumentCard from './DocumentCard';
@@ -193,6 +194,16 @@ function useFilesUploadForm(props: Props) {
   const onFileAdd = () => {
     if (disableChange) {
       return;
+    }
+
+    if (
+      props.type === 'photo' &&
+      files.length >= MAX_TRIP_PHOTOS &&
+      !(!files[files.length - 1].fileName && files.length === MAX_TRIP_PHOTOS)
+    ) {
+      return showErrorMessage(
+        `You can only upload maximum of ${MAX_TRIP_PHOTOS} photos!`,
+      );
     }
 
     if (files.length === 0 || files[files.length - 1]?.fileName) {
