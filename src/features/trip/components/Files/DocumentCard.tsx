84554e12caf97abaa72d57ce61b1586a714126bc
zacker-tech <127632508+zacker-tech@users.noreply.github.com>
2023-12-13 18:07:57 +0100

    fix: files form ui

--- src/features/trip/components/Files/DocumentCard.tsx
+++ src/features/trip/components/Files/DocumentCard.tsx
@@ -31,7 +31,7 @@ export default function DocumentCard({
         border: 1,
         borderRadius: 4,
         borderColor: 'grey.200',
-        width: 200,
+        width: { xs: 170, md: 200 },
         height: '100%',
       }}
     >
@@ -80,14 +80,25 @@ export default function DocumentCard({
             justifyContent="center"
             sx={{
               width: '100%',
-              height: 133,
+              height: { xs: 148, md: 133 },
               bgcolor: 'grey.100',
               borderRadius: 4,
             }}
           >
             <InsertDriveFileIcon sx={{ color: 'text.secondary' }} />
           </Stack>
-          <Typography color="text.primary">{name}</Typography>
+          <Typography
+            color="text.primary"
+            sx={{
+              overflow: 'hidden',
+              display: '-webkit-box',
+              '-webkit-line-clamp': '1',
+              'line-clamp': '1',
+              '-webkit-box-orient': 'vertical',
+            }}
+          >
+            {name}
+          </Typography>
         </Stack>
       </Stack>
     </Box>
--- src/features/trip/components/Files/FilesForm.tsx
+++ src/features/trip/components/Files/FilesForm.tsx
@@ -49,7 +49,7 @@ export default function FilesForm(props: Props) {
       onSubmit={handleSubmit(onSubmit)}
       noValidate
       direction="row"
-      gap={2}
+      gap={{ xs: 1, md: 2 }}
       flexWrap="wrap"
       sx={{ width: '100%' }}
     >
