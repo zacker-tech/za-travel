6f992f5e972f33fd834e6649eb51c282812f1c01
zacker-tech <127632508+zacker-tech@users.noreply.github.com>
2023-12-16 21:15:38 +0100

    feat: save wizard data to firestore (#14)

--- src/app/services/firebase/firebase.ts
+++ src/app/services/firebase/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
@@ -15,3 +16,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const storage = getStorage(app);
+export const firestore = getFirestore(app);
