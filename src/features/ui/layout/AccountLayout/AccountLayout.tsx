3f4dd4f60a4ff6743a190c5188b0ccc5247b8bc1
zacker-tech <127632508+zacker-tech@users.noreply.github.com>
2023-12-22 14:15:12 +0100

    fix: account layout height for the mobile

--- src/features/ui/layout/AccountLayout/AccountLayout.tsx
+++ src/features/ui/layout/AccountLayout/AccountLayout.tsx
@@ -84,8 +84,7 @@ export default function AccountLayout() {
       sx={{
         display: 'flex',
         bgcolor: 'grey.100',
-        minHeight: { md: '100vh' },
-        height: { xs: '100vh', md: 'auto' },
+        minHeight: '100vh',
         maxHeight: { xs: '-webkit-fill-available', md: 'auto' },
       }}
     >
