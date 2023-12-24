555f076c3c0db0b5f13d597e00096888b608e94e
zacker-tech <127632508+zacker-tech@users.noreply.github.com>
2023-12-24 22:39:20 +0100

    feat: add lazy loading of some pages

--- src/app/config/routes/components/AppRouter.tsx
+++ src/app/config/routes/components/AppRouter.tsx
@@ -1,46 +1,51 @@
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import Loader from '@features/ui/Loader';
 import AccountLayout from '@features/ui/layout/AccountLayout/AccountLayout';
 import AuthLayout from '@features/ui/layout/AuthLayout';
 import AddTripPage from '@pages/account/add-trip';
 import DashboardPage from '@pages/account/dashboard';
 import TripDetailsPage from '@pages/account/trips/trip-details';
 import TripsPage from '@pages/account/trips/trips';
-import HomePage from '@pages/home';
-import LoginPage from '@pages/login';
 import NotFoundPage from '@pages/not-found';
-import SignUpPage from '@pages/sign-up';
 
 import { AppRoutes } from '../AppRoutes';
 import ProtectedRoute from './ProtectedRoute';
 
+const HomePage = lazy(() => import('@pages/home'));
+const LoginPage = lazy(() => import('@pages/login'));
+const SignUpPage = lazy(() => import('@pages/sign-up'));
+
 export default function AppRouter() {
   return (
-    <Routes>
-      {/* Public Pages */}
-      <Route path={AppRoutes.home} element={<HomePage />} />
-      <Route element={<AuthLayout />}>
-        <Route path={AppRoutes.singUp} element={<SignUpPage />} />
-        <Route path={AppRoutes.login} element={<LoginPage />} />
-      </Route>
-      {/* Account Pages */}
-      <Route
-        element={
-          <ProtectedRoute>
-            <AccountLayout />
-          </ProtectedRoute>
-        }
-      >
-        <Route path={AppRoutes.dashboard} element={<DashboardPage />} />
-        <Route path={AppRoutes.trips} element={<TripsPage />} />
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        {/* Public Pages */}
+        <Route path={AppRoutes.home} element={<HomePage />} />
+        <Route element={<AuthLayout />}>
+          <Route path={AppRoutes.singUp} element={<SignUpPage />} />
+          <Route path={AppRoutes.login} element={<LoginPage />} />
+        </Route>
+        {/* Account Pages */}
         <Route
-          path={`${AppRoutes.trips}/:tripId`}
-          element={<TripDetailsPage />}
-        />
-        <Route path={AppRoutes.addTrip} element={<AddTripPage />} />
-      </Route>
-      {/* Not Found Page */}
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+          element={
+            <ProtectedRoute>
+              <AccountLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route path={AppRoutes.dashboard} element={<DashboardPage />} />
+          <Route path={AppRoutes.trips} element={<TripsPage />} />
+          <Route
+            path={`${AppRoutes.trips}/:tripId`}
+            element={<TripDetailsPage />}
+          />
+          <Route path={AppRoutes.addTrip} element={<AddTripPage />} />
+        </Route>
+        {/* Not Found Page */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 }
